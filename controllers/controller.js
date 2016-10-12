46de2261923661b4e48964ee96a9d222f5b4adc0
Stan Lee <tangjianyi1993@live.com>
2016-10-12 17:34:39 +0800

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,20 @@
+var controllCategory = {
+    '/initTags.node'    : 'viewSqlInitTags',
+    '/initLatest.node'  : 'viewSqlInitLatest',
+    '/initTimeline.node': 'viewSqlInitTimeline',
+    '/initCategory.node': 'viewSqlInitCategory',
+
+    '/categoryByTagName.node' : 'viewSqlCategoryByTagName',
+    '/categoryByTimeline.node': 'viewSqlCategoryByTimeline',
+
+    '/paperById.node': 'viewSqlPaperById'
+};
+
 var controll = function(request, response) {
-    switch (request.path) {
-        default :
-            
-    }
+    var callbackFunc  = function(resultObj) { response.json(resultObj); },
+        viewSqlModule = require('../models/' + controllCategory[request.path] +'.js');
+    
+    viewSqlModule.sqlQuery(request, callbackFunc);
 }
 
 module.exports.controll = controll;
